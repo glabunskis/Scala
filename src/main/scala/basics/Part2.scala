3269b900be2a8f6cf5f7f1b96f3456edbaa604ac
Glebs Labunskis <G.Labunskis@student.tudelft.nl>
2021-09-22 01:22:10 +0300

    Part 2 complete

--- src/main/scala/basics/Part2.scala
+++ src/main/scala/basics/Part2.scala
@@ -1,5 +1,11 @@
 package basics
 
+import org.json4s.JsonDSL.int2jvalue
+import sun.font.TrueTypeFont
+
+import java.util.Optional
+import scala.collection.immutable.Nil.:::
+
 /**
  * PART 2 - LISTS AND PATTERN MATCHING
  *
@@ -35,8 +41,8 @@ object Part2 {
      */
     val h = three.head
     val t = three.tail
-    println(s"Head of $three is $h")
-    println(s"Tail of $three is $t")
+//    println(s"Head of $three is $h")
+//    println(s"Tail of $three is $t")
 
     /* Note 1
      * the two println statements above use something called String
@@ -55,8 +61,8 @@ object Part2 {
      */
 
     val plusOne = three.map(x => x + 1)
-    println(three) // The original list is not changed
-    println(plusOne) // This is a new list
+//    println(three) // The original list is not changed
+//    println(plusOne) // This is a new list
 
 
     /**
@@ -68,7 +74,7 @@ object Part2 {
 
     // calling `println(sum(four))` displays the result of the function sum
     // which is defined using pattern matching below
-    println(sum(four)) // 10
+    //println(sum(four)) // 10
 
     val drivers = List(
       ("green", 14),
@@ -82,8 +88,16 @@ object Part2 {
      * the output of calling the traffic light function on drivers
      * N.B. the type signature of the function and how the input is mapped
      */
-    println(drivers.map(arg => trafficLightPenalty(arg._1, arg._2)))
-
+    //println(drivers.map(arg => trafficLightPenalty(arg._1, arg._2)))
+    //println(twice(List()))
+    //println(drunkWords(List("Hey","you,","how","are","you","doing?")))
+    //val startsWithS = (s: String) => s.startsWith("s") // lambda expression
+    //println(myForAll(List("sbc", "sef"), startsWithS))
+    //println(lastElem(List())) // None
+    //println((lastElem(List.range(0,3))))// Some(2) (range has exclusive ceiling))
+    //println(append(List(), List())) 	// List(1,3,5,2,4)
+    val nrs = List.range(0,11) // List(0,1,2,3,...,10)
+    println(myFilter(nrs, (i: Int) => i % 2 == 0))// List(0,4,8)
   }
 
   /**
@@ -202,7 +216,10 @@ object Part2 {
    *           Example: twice(List.range(0,4)) // List(0, 0, 1, 1, 2, 2, 3, 3)
    */
   //TODO: supply the method body of twice, using pattern matching
-  def twice[A](xs: List[A]): List[A] = ???
+  def twice[A](xs: List[A]): List[A] = xs match {
+    case Nil =>  List()
+    case a :: t => a :: a :: twice(t)
+  }
 
   /** Q5 (2p)
    * You had a few drinks too much after a party and recorded a message for
@@ -216,7 +233,14 @@ object Part2 {
    * List("?gniod","ouy","era","woh",",ouy","yeH")
    */
   // TODO: supply the method body of drunkWords using pattern matching
-  def drunkWords(xs: List[String]): List[String] = ???
+  def drunkWords(xs: List[String]): List[String] = xs match {
+//    def reverse(xs: String): String =
+
+    case Nil => List()
+    case xs :: t => drunkWords(t) ::: List(xs.reverse)
+  }
+
+
 
   /** Q6 (3p)
    * MyForAll takes a list of elements, and applies a function to it, to
@@ -229,7 +253,10 @@ object Part2 {
    * myForAll(List("start", "strong", "system"), startsWithS) 	// true
    */
   // TODO: supply the method body of myForAll using pattern matching.
-  def myForAll[A](xs: List[A], f: A => Boolean): Boolean = ???
+  def myForAll[A](xs: List[A], f: A => Boolean): Boolean = xs match {
+    case Nil => true
+    case xs :: t => f(xs) & myForAll(t, f)
+  }
 
   /** Q7 (3p)
    * This is the first question where you encounter the Option[T] type
@@ -245,7 +272,11 @@ object Part2 {
    *         lastElem(List.range(0,3)) // Some(2) (range has exclusive ceiling)
    */
   // TODO implement using pattern matching
-  def lastElem[A](xs: List[A]): Option[A] = ???
+  def lastElem[A](xs: List[A]): Option[A] = xs match {
+    case xs :: Nil => Some(xs)
+    case xs :: t => lastElem(t)
+    case Nil  => None
+  }
 
   /** Q8 (3p)
    * Take two lists and concatenate them, returning the result
@@ -259,7 +290,7 @@ object Part2 {
    *         append(List(1,3,5), List(2,4)) 	// List(1,3,5,2,4)
    */
   // TODO implement using pattern matching
-  def append[A](xs: List[A], ys: List[A]): List[A] = ???
+  def append[A](xs: List[A], ys: List[A]): List[A] = xs :::ys
 
   /**
    * Q9 (5p)
@@ -283,7 +314,16 @@ object Part2 {
    */
   // TODO implement method using pattern matching
   // a helper method which you've written yourself
-  def myFilter[A](xs: List[A], f: A => Boolean): List[A] = ???
+  def helper[A](xs: List[A], f: A => Boolean, index: Int ): List[A] = xs match {
+    case Nil => List()
+    case x :: t=> if(f(x)) {
+      if (index % 2 == 1) x :: helper(t,f,index+1) else helper(t,f,index+1)
+    } else helper(t,f,index)
+  }
+  def myFilter[A](xs: List[A], f: A => Boolean): List[A] = xs match{
+    case Nil => List()
+    case x => helper(x,f,1)
+  }
 
   // END OF PART 2
 }
